e5dc156a87ea15502cb6e587ca0feedc719d64e0
Mikhail Makar <makar.mikhail@yandex.ru>
2018-11-01 15:48:54 +0300

    add method: update; fixed methods: save, delete, get;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,52 +3,83 @@
  */
 public class ArrayStorage {
 
-    Resume[] storage = new Resume[10000];
-    int count = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
-        for (int i = 0; i < count; i++){
+    public void clear(){
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
-        count = 0;
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[count] = r;
-        count++;
+        if (size >= 0 && size <= storage.length){
+            storage[size] = r;
+            size++;
+            for (int i = 0; i < size - 1; i++){
+                if (storage[i].getUuid().equals(r.getUuid())){
+                    storage[size] = null;
+                    System.out.println("Resume already exists!");
+                    size--;
+                }
+            }
+        } else System.out.println("Storage is over!");
     }
 
-    Resume get(String uuid) {
-        for (Resume aStorage : storage){
-            if (aStorage != null && aStorage.uuid.equals(uuid)) {
-                return aStorage;
+    public void update(Resume r) {
+        if (checkResumeInStorage(r.getUuid())) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(r.getUuid())) {
+                    storage[i] = new Resume();
+                    storage[i].setUuid(r.getUuid() + "updated");
+                }
             }
-        }
+        } else System.out.println("Resume aren't exist in the storage!");
+    }
+
+    public Resume get(String uuid) {
+        if (checkResumeInStorage(uuid)) {
+            for (Resume aStorage : storage) {
+                if (aStorage != null && aStorage.getUuid().equals(uuid)) {
+                    return aStorage;
+                }
+            }
+        } else System.out.println("Resume aren't exist in the storage!");
         return null;
     }
 
-    void delete(String uuid) {
-        for(int i = 0; i < count; i++){
-            if (storage[i].uuid.equals(uuid)){
-                count--;
-                while (i < count){
-                    storage[i] = storage[i+1];
-                    i++;
+    public void delete(String uuid) {
+        if (checkResumeInStorage(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    size--;
                 }
             }
-        }
+        } else System.out.println("Resume aren't exist in the storage!");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        Resume[] getAllResume = new Resume[count];
+    public Resume[] getAll() {
+        Resume[] getAllResume = new Resume[size];
         System.arraycopy(storage, 0, getAllResume, 0, getAllResume.length);
         return getAllResume;
     }
 
-    int size() {
-        return count;
+    public int size() {
+        return size;
+    }
+
+    private boolean checkResumeInStorage(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
