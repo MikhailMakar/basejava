d1128e7799b1a65360cd2fbdb26fd8d8e0c5fbda
Mikhail Makar <makar.mikhail@yandex.ru>
2018-11-04 00:38:08 +0300

    made all methods in SortedArrayStorage

--- src/com/basejava/webapp/storage/SortedArrayStorage.java
+++ src/com/basejava/webapp/storage/SortedArrayStorage.java
@@ -8,27 +8,53 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public void update(Resume r) {
-
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index] = r;
+            sortStorage(storage);
+        } else {
+            System.out.println("Resume aren't exist in the storage!");
+        }
     }
 
     @Override
     public void save(Resume r) {
-
+        if (size < STORAGE_LIMIT) {
+            if (getIndex(r.getUuid()) >= 0) {
+                System.out.println("Resume is already exist!");
+            } else {
+                storage[size] = r;
+                size++;
+                sortStorage(storage);
+            }
+        } else {
+            System.out.println("Storage is over!");
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            while (storage[index] != null){
+                storage[index] = storage[index + 1];
+                index++;
+            }
+            size--;
+        } else {
+            System.out.println("Resume aren't exist in the storage!");
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     @Override
@@ -36,5 +62,17 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+
+    private void sortStorage(Resume[] resumes){
+        for (int i = size - 1; i > 0; i--){
+            for (int j = 0; j < i; j++){
+                if (resumes[j].compareTo(resumes[j+1]) > 0){
+                    Resume resume = resumes[j];
+                    resumes[j] = resumes[j+1];
+                    resumes[j+1] = resume;
+                }
+            }
+        }
     }
 }
