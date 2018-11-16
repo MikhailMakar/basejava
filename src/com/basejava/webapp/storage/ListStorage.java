2c55312bebeb5365c38d99d360c06851dbb69a94
Mikhail Makar <makar.mikhail@yandex.ru>
2018-11-16 13:27:58 +0300

    finished ListStorage, added Test Classes

--- src/com/basejava/webapp/storage/ListStorage.java
+++ src/com/basejava/webapp/storage/ListStorage.java
@@ -1,5 +1,8 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.ExistStorageException;
+import com.basejava.webapp.exception.NotExistStorageException;
+import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
 import java.util.LinkedList;
@@ -17,22 +20,67 @@ public class ListStorage extends AbstractStorage {
 
     @Override
     public void update(Resume r) {
-
+        boolean flag = false;
+        if (r != null) {
+            for (Resume resume : listResume) {
+                flag = r.getUuid().equals(resume.getUuid());
+                if (flag) {
+                    int index = listResume.indexOf(resume);
+                    listResume.add(index, r);
+                }
+            }
+            if (!flag) {
+                throw new NotExistStorageException(r.getUuid());
+            }
+        } else {
+            throw new NullPointerException("Object is null");
+        }
     }
 
     @Override
     public void save(Resume r) {
+        String id = r.getUuid();
+        if (listSize < STORAGE_LIMIT) {
+            if (id != null) {
+                for (Resume resume : listResume) {
+                    if (id.equals(resume.getUuid())) {
+                        throw new ExistStorageException(id);
+                    }
+                }
+                listResume.add(r);
+            } else {
+                throw new NullPointerException("uuid is null!");
+            }
+        } else {
+            throw new StorageException("Storage overflow", id);
+        }
 
     }
 
     @Override
     public Resume get(String uuid) {
-        return null;
+        for (Resume resume : listResume) {
+            if (uuid.equals(resume.getUuid())) {
+                int index = listResume.indexOf(resume);
+                return listResume.get(index);
+            }
+        }
+        throw new NotExistStorageException(uuid);
     }
 
     @Override
     public void delete(String uuid) {
-
+        boolean flag = false;
+        for (Resume resume : listResume) {
+            flag = uuid.equals(resume.getUuid());
+            if (flag) {
+                int index = listResume.indexOf(resume);
+                listResume.remove(index);
+            }
+        }
+        if (!flag) {
+            throw new NotExistStorageException(uuid);
+        }
     }
 
     @Override
---
+++ test/com/basejava/webapp/storage/MapStorageTest.java
@@ -0,0 +1,34 @@
+package com.basejava.webapp.storage;
+
+import org.junit.Test;
+
+public class MapStorageTest {
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void size() {
+    }
+}
